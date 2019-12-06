ea36fa015a21a54ec6ccd0822c7db2d519c66082
Terra Williams <terraw9@gmail.com>
2019-12-06 11:24:27 -0800

    Created singletone user data class

---
+++ RecipeVirtuoso/MyRecipesTabModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeVirtuoso
+{
+    class MyRecipesTabModel
+    {
+        #region Members
+
+        #endregion
+
+        #region Getters and Setters
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Methods
+        public void AddRecipe()
+        {
+
+        }
+
+        public void AddIngredient()
+        {
+
+        }
+
+        public void AddRecipeTask()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ RecipeVirtuoso/UserData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeVirtuoso
+{
+    class UserData
+    {
+        #region Members
+        private static UserData instance;
+
+        private static List<Recipe> userRecipes = new List<Recipe>();
+        private static List<Meal> userMeals = new List<Meal>();
+        #endregion
+
+        private UserData() { }
+
+        public static UserData Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new UserData();
+
+                return instance;
+            }
+        }
+
+        public static List<Recipe> UserRecipes
+        {
+            get { return userRecipes; }
+        }
+
+        public static List<Meal> UserMeals
+        {
+            get { return userMeals; }
+        }
+    }
+}
