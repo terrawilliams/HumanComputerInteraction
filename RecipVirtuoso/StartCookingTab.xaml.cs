ae218c0354a5567bf27c4fd22f6af763fac978a2
Derek Stratton <derekdstratton@gmail.com>
2019-12-05 19:11:00 -0800

    Fix misspelling lol

--- RecipVirtuoso/StartCookingTab.xaml.cs
+++ RecipVirtuoso/StartCookingTab.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RecipVirtuoso
+namespace RecipeVirtuoso
 {
     /// <summary>
     /// Interaction logic for StartCookingTab.xaml
