43756495fecb1e40438a13213e731a993246f0c3
Derek Stratton <derekdstratton@gmail.com>
2019-12-05 19:06:37 -0800

    The algorithm now handles passive vs active

--- RecipVirtuoso/Meal.cs
+++ RecipVirtuoso/Meal.cs
@@ -10,6 +10,7 @@ namespace RecipVirtuoso
     class Meal
     {
         private List<Recipe> recipes = new List<Recipe>();
+        private List<RecipeTask> sortedRecipeTasks = new List<RecipeTask>();
 
         public void addRecipe(Recipe r)
         {
@@ -23,7 +24,6 @@ namespace RecipVirtuoso
 
         public void cook()
         {
-            List<RecipeTask> goodList = new List<RecipeTask>();
             //terra: i have a gigantic brain
             int numRecipes = recipes.Count;
             int [] tasksPerRecipe = new int[numRecipes];
@@ -31,8 +31,8 @@ namespace RecipVirtuoso
             {
                 tasksPerRecipe[i] = recipes[i].getNumTasks();
             }
+            //an array of total time remaining at each step. Each row is a recipe, each column is the time to finish.
             int [,] totalTimeRemaining = new int[numRecipes,tasksPerRecipe.Max()];
-            bool [,] actives = new bool[numRecipes, tasksPerRecipe.Max()];
             int lastTime;
             for (int i = 0; i < numRecipes; i++)
             {
@@ -41,23 +41,50 @@ namespace RecipVirtuoso
                 {
                     totalTimeRemaining[i, j] = recipes[i].getTasks()[j].getTime() + lastTime;
                     lastTime = totalTimeRemaining[i, j];
-                    actives[i, j] = recipes[i].getTasks()[j].getActive();
                 }
             }
+            //these are iterators that increment for each recipe's task
             int [] taskIterators = new int[numRecipes];
             for (int i = 0; i < numRecipes; i++)
             {
                 taskIterators[i] = 0;
+            }
+            //these are holds put on a recipe's task while a passive task is happening. 0 is no hold
+            int [] holds = new int[numRecipes];
+            for (int i = 0; i < numRecipes; i++)
+            {
+                holds[i] = 0;
             }
             //Here's the juicy part
             while (!Enumerable.SequenceEqual(tasksPerRecipe, taskIterators))
             {
                 int longestTimeRemaining = 0;
                 int longestTimeIndex = -1;
+
+                //If all recipes that aren't finished are on hold, then set the lowest hold to 0.
+                int lowestHold = Int32.MaxValue;
+                int lowestHoldIndex = -1;
                 for (int i = 0; i < numRecipes; i++)
                 {
-                    if (taskIterators[i] < tasksPerRecipe[i]) //make sure that recipe isnt done
+                    //make sure recipe isn't done
+                    if (taskIterators[i] < tasksPerRecipe[i])
                     {
+                        if (holds[i] < lowestHold)
+                        {
+                            lowestHold = holds[i];
+                            lowestHoldIndex = i;
+                        }
+                    }
+                }
+
+                holds[lowestHoldIndex] = 0;
+
+                for (int i = 0; i < numRecipes; i++)
+                {
+                    //make sure that recipe isn't done or on hold
+                    if (taskIterators[i] < tasksPerRecipe[i] && holds[i] == 0) 
+                    {
+                        //compute max
                         if (totalTimeRemaining[i, taskIterators[i]] > longestTimeRemaining)
                         {
                             longestTimeRemaining = totalTimeRemaining[i, taskIterators[i]];
@@ -65,11 +92,23 @@ namespace RecipVirtuoso
                         }
                     }
                 }
-                goodList.Add(recipes[longestTimeIndex].getTasks()[taskIterators[longestTimeIndex]]);
+
+                //This is the recipe task to select to add.
+                RecipeTask recipeTask = recipes[longestTimeIndex].getTasks()[taskIterators[longestTimeIndex]];
+                sortedRecipeTasks.Add(recipeTask);
+
+                //update all of the holds
+                for (int i = 0; i < numRecipes; i++)
+                {
+                    holds[i] = Math.Max(0, holds[i] - recipeTask.getTime());
+                }
+                //if the task is passive, add a hold
+                holds[longestTimeIndex] = !recipeTask.getActive() ? recipeTask.getTime() : 0;
+
                 taskIterators[longestTimeIndex]++; //iterate to next task for that recipe
             }
             Console.WriteLine("Sorted List:");
-            foreach (var recipeTask in goodList)
+            foreach (var recipeTask in sortedRecipeTasks)
             {
                 Console.WriteLine(recipeTask.getTime());
             }
